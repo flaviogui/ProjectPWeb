d74c7e7293ba14aaf5effca8a657cd92b388b8c1
flaviogui <flavioglaydson-pc@hotmail.com>
2023-12-16 16:43:23 -0300

    Tela add atualizada

--- receitas-culinarias/src/app/componentes/recipeForm/recipeForm.tsx
+++ receitas-culinarias/src/app/componentes/recipeForm/recipeForm.tsx
@@ -14,8 +14,34 @@ export default function RecipeForm(){
         <textarea className={Styles.textarea} placeholder="Ingredientes"></textarea>
         <textarea className={Styles.textarea} placeholder="Modo de Preparo"></textarea>
         <textarea className={Styles.textarea} placeholder="Descrição"></textarea>
-        <Button>Salvar</Button>
+        <label className={Styles.categoryLabel} htmlFor="category">CATEGORIA:</label>
+        <select className={Styles.categorySelect} name="category" id="category">
+        <option value="cafemanha">Café da manha</option>
+        <option value="almoco">Almoço</option>
+        <option value="janta">Janta</option>
+        <option value="carnes">Carnes</option>
+        <option value="doce">Doces e Sobremesa</option>
+        <option value="peixe">Peixes de água doce</option>
+        <option value="lanche">Lanche</option>
+        <option value="tiragosto">Tira Gosto</option>
+        <option value="vegetatiano">Comidas Vegetarianas</option>
+        </select>
+        <Button>ADICIONAR</Button>
     </ol>
 </div>
   );
 }
+
+/*
+-café da manha                                                                                                                                       
+-almoco
+-janta
+-bebidas
+-massas
+-carnes
+-doces e sobremesa
+-peixes e fruto do mar
+-lanche
+-vegetariano 
+-tira gosto
+*/
