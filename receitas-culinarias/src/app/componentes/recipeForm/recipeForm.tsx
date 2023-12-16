458984c0f29dbfad4f2ac3c2252e60db238b1aa0
flaviogui <flavioglaydson-pc@hotmail.com>
2023-12-16 14:14:03 -0300

    AddReceita quase pronto

--- receitas-culinarias/src/app/componentes/recipeForm/recipeForm.tsx
+++ receitas-culinarias/src/app/componentes/recipeForm/recipeForm.tsx
@@ -8,12 +8,12 @@ export default function RecipeForm(){
     <ol className={Styles.ol}>
         <h1 className={Styles.title}>ADICIONAR RECEITA</h1>
         <Input type="name" placeholder="Nome da receita:" />
-        <Input type="text" placeholder="Descrição" />
-        <textarea className={Styles.textarea} placeholder="Ingredientes"></textarea>
-        <textarea className={Styles.textarea} placeholder="Modo de Preparo"></textarea>
         <Input type="name" placeholder="Nome do autor" />
         <Input type="name" placeholder="Tempo de Preparo" />
         <Input type="text" placeholder="URL" />
+        <textarea className={Styles.textarea} placeholder="Ingredientes"></textarea>
+        <textarea className={Styles.textarea} placeholder="Modo de Preparo"></textarea>
+        <textarea className={Styles.textarea} placeholder="Descrição"></textarea>
         <Button>Salvar</Button>
     </ol>
 </div>
