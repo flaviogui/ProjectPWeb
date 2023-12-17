646c655ef14eef0fde893f2284dad11a4f1e7db7
flaviogui <flavioglaydson-pc@hotmail.com>
2023-12-17 10:01:01 -0300

    Header extra

---
+++ receitas-culinarias/src/app/componentes/header/header3.tsx
@@ -0,0 +1,16 @@
+import Link from 'next/link'
+import styles from './header.module.css'
+
+export default function Header(){
+    return(
+            <nav className={styles.nav}>
+              <Link className= {styles.title} href="/">CULINÁRIA NODESTINA</Link>
+              <form className={styles.form}>
+                <input className={styles.input} type= "text"/>
+                <button className={styles.button}>Pesquisar</button>
+              </form>
+              <Link href="/addReceita" className={styles.loginLink}>
+              CADASTRAR RECEITA</Link>
+            </nav>
+    )
+}
