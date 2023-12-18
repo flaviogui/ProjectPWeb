9e716b97f6d725f3f9d011c1a443170903c9cdee
flaviogui <flavioglaydson-pc@hotmail.com>
2023-12-18 15:27:09 -0300

    para teste

---
+++ receitas-culinarias/src/app/addReceita2/page.tsx
@@ -0,0 +1,10 @@
+import AdicionarReceita from '../componentes/recipeForm/recipeForm'
+import Heard from '../componentes/header/header2'
+export default function AddReceita(){
+  return (
+    <div>
+      <Heard/>
+      <AdicionarReceita/>
+    </div>
+  )
+}
